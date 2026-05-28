ce3d44e623400b03b593e784e37442a914de3bef
Carson-Marden12 <mardenboy55@gmail.com>
2026-05-28 10:41:34 -0500

    Lots of fabulous code that will for sure impress the person that is looking at it

---
+++ twentyFourtyEight.java
@@ -0,0 +1,32 @@
+public class twentyFourtyEight {
+    
+
+    public static void main(String[] args) {
+        int[][] board = new int[4][4];
+        int rx = (int) Math.random() * 4;
+        int ry = (int) Math.random() * 4;
+        int rz = (int) Math.random() * 10;
+        if(rz < 9) {
+            board[rx][ry] = 2;
+
+        } else {
+             board[rx][ry] = 4;
+        }  
+
+        rx = (int) Math.random() * 4;
+        ry = (int) Math.random() * 4;
+        rz = (int) Math.random() * 10;
+        if(rz < 9) {
+            board[rx][ry] = 2;
+
+        } else {
+             board[rx][ry] = 4;
+        }  
+        
+        for(int x = 0; x < board.length; x ++){
+            for(int y = 0; y < board[x].length; y++) {
+               
+            }
+        }
+    }
+}
